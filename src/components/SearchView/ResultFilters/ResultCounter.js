e839b0845022d313d9377242d11630dc0c5e165f
Eddie Arevalo <eddie.arevalo@jpl.nasa.gov>
2019-11-22 15:23:02 -0800

    Added Result Counter Functionality
    -Result counter will now show the correct number of results found.

--- src/components/SearchView/ResultFilters/ResultCounter.js
+++ src/components/SearchView/ResultFilters/ResultCounter.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
  
 class ResultCounter extends Component {
     componentDidMount() {
     }
     render() {
+        let startNumber = this.props.appReducer.start + 1;
+        let endNumber = startNumber + this.props.appReducer.rows;
+        if(this.props.appReducer.searchResults.response && this.props.appReducer.searchResults.response.docs){
+            endNumber = startNumber + this.props.appReducer.searchResults.response.docs.length - 1;
+        }
+        let resultsCount = -1;
+        if(this.props.appReducer.searchResults.response && this.props.appReducer.searchResults.response.numFound){
+            resultsCount = this.props.appReducer.searchResults.response.numFound;
+        }
+
         return (
             <div className="resultFilter">
-                Showing 1-50 of 5510 Results
+                Showing {startNumber}-{endNumber} of {resultsCount} Results
             </div>
         );
     }
 }
+
+const mapStateToProps = state => ({
+    ...state
+});
  
-export default ResultCounter;
+export default connect(mapStateToProps, null)(ResultCounter);  
