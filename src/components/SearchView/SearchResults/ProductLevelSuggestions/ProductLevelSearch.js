734515140ad55d3be91c4529886d79ccc674f4ce
Eddie Arevalo <eddiesarevalo@gmail.com>
2020-07-13 17:07:28 -0700

    Remove Bulma CSS #11
    -Removed bulma css.
    -Removed app styling.

--- src/components/SearchView/SearchResults/ProductLevelSuggestions/ProductLevelSearch.js
+++ src/components/SearchView/SearchResults/ProductLevelSuggestions/ProductLevelSearch.js
@@ -10,7 +10,6 @@ class ProductLevelSearch extends Component {
                 <div className="card">
                     <div className="card-image">
                         <figure className="image">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
                         </figure>
                     </div>
                     <div className="card-content">
